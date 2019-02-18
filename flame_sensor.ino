21a4a27be04ee4f2cae2658a43bf378309446ccc
Denis Kardakov <37693576+DenisKardakov@users.noreply.github.com>
2019-02-18 18:04:54 +0300

    Add files via upload
    Analog/digital flame sensor

---
+++ flame_sensor.ino
@@ -0,0 +1,22 @@
+
+int sensorPin = A0; // select the input pin for the LDR
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Getting data");
+  sensorValue = analogRead(sensorPin); // получаем аналоговый сигнал
+  Serial.println(sensorValue);
+    if (sensorValue < 100)
+      {
+        Serial.println("Fire Detected");
+        delay(1000);
+      }
+  delay(sensorValue);
+
+}
