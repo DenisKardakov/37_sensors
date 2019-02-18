4d9139a81291563f57085afe77e687dd59775437
Denis Kardakov <37693576+DenisKardakov@users.noreply.github.com>
2019-02-18 18:08:40 +0300

    DHT11/22 Humidity and Temperature sensor

---
+++ dht22.ino
@@ -0,0 +1,43 @@
+#include "DHT.h"
+// For DHT 22 (pins numbered from left to right):
+// pin 1 - 5V
+// pin 2 - data
+// pin 3 - 3.3 V (do not connect)
+// pin 4 - GND 
+#define DHTPIN 4 // data pin number 
+
+// Choose sensor version below
+
+// Sensor initialization
+DHT dht(DHTPIN, DHT11);
+
+
+void setup() {
+   
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+   // 2 second delay between measurements
+  delay(2000);
+  
+  //Get humidity
+  
+  float h = dht.readHumidity();
+  
+  // get temperature
+  
+  float t = dht.readTemperature();
+  
+  // Check if get is succesfull
+  
+    
+  Serial.print("Humidity : ");
+  Serial.println (h);
+  Serial.print(" Temp: ");
+  Serial.println(t);
+
+}
+
+
